117cf8db43d80bfc249b84080ab4eb221d6c22b4
mohamed chaabi <chaabi.mohamed@ofppt-edu.ma>
2023-05-24 09:12:02 +0100

    add search by image, license, installation

--- main.py
+++ main.py
@@ -7,8 +7,12 @@ from pydantic import BaseModel
 import uuid
 import os
 from PIL import Image
+from DeepImageSearch import SearchImage
+import json
+from bson.json_util import dumps
 
-#import collection (table from mongodb)
+
+#to use collection from mongodb
 from mongoDB.mongodb import collection
 
 #generate listing id
@@ -32,42 +36,71 @@ class CarInformation(BaseModel):
 async def add_new_listing(
     name: str = Form(...),
     price: float = Form(...),
-    files: List[UploadFile] = File(...),
+    images: List[UploadFile] = File(...),
     ):
-    
+
     #get new listing data from request
     id = get_uuid_id()
 
-    #upload file to "sscar/cars/{listing id}"
-    listingPath = f'sscar/cars/{id}/'
-    os.mkdir(listingPath)
-    for image in files:
-        img = Image.open(image.file)
-        path = listingPath+image.filename
-        img.save(path)
+    #save images into folder
+    listing_path = f'sscar/cars/{id}/'
+    save_images_into_folder(listing_path,images)
 
-    #save data to a mongodb database
-    newListing = CarInformation(
+    #new object
+    new_listing = CarInformation(
         id=id,
         name=name,
         price=price, 
-        images= [listingPath+file.filename for file in files]
+        images= [listing_path+file.filename for file in images]
     )
-    
-    #save to collection
-    collection.insert_one(jsonable_encoder(newListing))
+    #save new object to collection
+    save_data_to_mongodb(new_listing)
 
     return {
-        "Ok! car add to sscar" : newListing,
+        "Ok! car add to sscar" : new_listing,
     }
 
 #search algo
 @app.post('/search')
 async def search_by_img(image: UploadFile):
-    try:
-        return {"Your Image":image.filename, "Result":"ok!"}
-    except:
-        return {"some error here try later!"}
+    #save image
+    path = save_image(image)
+    #get similar image ids
+    car_ids=get_similar_result(path,10)
+    #get cars information from mongodb
+    result = get_data_from_mongodb(car_ids)
+    print(result)
+    return {"result":result}
+    
+##functions
+def save_images_into_folder(listing_path,images):
+    os.mkdir(listing_path)
+    for image in images:
+        img = Image.open(image.file)
+        path = listing_path+image.filename
+        img.save(path)
+    # indexing listing
+
+def save_data_to_mongodb(new_listing):    
+    collection.insert_one(jsonable_encoder(new_listing))
+
+def save_image(image):
+    queryImagesPath = f'sscar/query/'
+    img = Image.open(image.file)
+    path = queryImagesPath+image.filename
+    img.save(path)
+    return path
+
+def get_similar_result(path,nbr_of_image):
+    img_list = SearchImage().get_similar_images(image_path=path,number_of_images=nbr_of_image)
+    imge_links = [img_list[id] for id in img_list.keys()]
+    cars = [car.split('/')[2] for car in imge_links]
+    cars = list(set(cars)) #remove duplicate ids
+    return cars
+
+def get_data_from_mongodb(ids):
+    result = collection.find({"id":{"$in":ids}})
+    return json.loads(dumps(result))
 
 if __name__ == '__main__':
     app.run(debug=True)
--- readme.md
+++ readme.md
@@ -1,13 +1,48 @@
-# SSCAR Image Search API V1.0
-### FastAPI | MongoDB
+# SSCar Image Search API V1.0
+## Technology stack
+**FastAPI | MongoDB**
+## Description
+SSCar Image Search API is an image search provider that can manage image and index searches.
+#### The goal is to have image search incorporated easily into any web application.
+### Features:
+1. Adding listing information and images.
+1. Searching by image.
+1. Indexing new listing images.
+## Requirement
+**In our case SSCar A Graduation Project**
+1. User can add car information and images.
+1. Users can search by image.
+## Installation
+1. Clone repo
+```bash
+    git clone https://github.com/medChaabi/sscar-image-search-api.git
+```
+2. Go into the repo
+```bash
+    cd sscar-image-search-API
+```
+3. Setup virtual environment
+```bash
+    pip install virtualenv # Install virtual environment
+    python3 -m venv venv # Create new virtual environment named venv
+    source env/bin/activate # Activate venv
+    pip install -r requirements.txt # Install SSCar-image-search-API requirements
+```
+4. Run the developement server
+```bash 
+    uvicorn main:app --reload
+```
+5. Start Making This more exciting [Swagger UI](http://127.0.0.1:8000/docs)
+```
+http://127.0.0.1:8000/docs
+```
+## Todo Next
+-  Optimize performance.
+-  Make it a feature that can any site add it.
 
-**Requirement**
-- user can add car (information and images)
-- user can search by image
+##
 
 
-
-**Instalation**
-
-
-**Todo Next**
+## If You Can Help | If this API helps you ;)
+[Email Me](med.chaabi98@gmail.com) <br>
+[SSCar Team]()
